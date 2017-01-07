845bb2c503e5c862654a15507b6f40aa2de6957d
Zining Zhu <zhuzining@126.com>
2017-01-06 22:35:46 -0500

    023

--- 023-merge-k-sorted-lists/Solution.java
+++ 023-merge-k-sorted-lists/Solution.java
@@ -9,45 +9,43 @@
 public class Solution {
 
     public ListNode mergeKLists(ListNode[] lists) {
-		/** The online priority queue solution. Just cast everything into a priority queue, and then perform N ExtractMin() operations. O(NlogN) time and O(N) space. Note that this does not even need the k inputs to be sorted.
-		**/
-		// FIXME - memory limit exceeded when doing [[-2,-1,-1,-1],[]]
 		if (lists.length == 0) {
-			return null;
+		    return null;
 		}
-
+		ListNode res = new ListNode(0);
 		PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
-			public int compare(ListNode o1, ListNode o2) {
-				if (o1.val < o2.val) return -1;
-				else if (o1.val == o2.val) return 1;
-				else return 1;
-			}
+		    public int compare(ListNode n1, ListNode n2) {
+		        if (n1.val > n2.val) {
+		            return 1;
+		        } else if (n1.val == n2.val) {
+		            return 0;
+		        } else {
+		            return -1;
+		        }
+		    }
 		});
 
-		ListNode res = new ListNode(0);
 
-		ListNode curr = lists[0];
-		for (int i = 0; i < lists.length; i++) {
-			curr = lists[i];
-			if (curr == null) continue;
-			while (curr.next != null) {
-			    pq.offer(curr);
-			    curr = curr.next;
-			}
-			pq.add(curr);
-
-
+		int num = 0;
+		for (ListNode l : lists) {
+		    if (l == null) continue;
+		    for (ListNode curr = l; curr != null; curr = curr.next) {
+		        pq.offer(curr);
+		        num++;
+		    }
 		}
-        //System.out.println("Size of pq: " + pq.size());
-        curr = res;
-		while (pq.size() > 0) {
-			ListNode p = pq.remove();
-			curr.next = p;
-			curr = curr.next;
-			//System.out.println("Size of pq: " + pq.size());
+
+		ListNode c = res;
+		while (pq.size() != 0) {
+		    c.next = pq.poll();
+		    num--;
+		    c = c.next;
+		    c.next = null; // Important! Otherwise there may be time limitation exceed, since you can't guarantee that the last node of res list has null next attribute.
 		}
-		res = res.next;
-		return res;
+
+
+
+		return res.next;
 
 	}
 }
