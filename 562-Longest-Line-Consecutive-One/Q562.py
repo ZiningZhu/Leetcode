826be07967471f184ff5a381adaa954cfd39229f
ZiningZhu <zhuzining@126.com>
2017-04-23 11:57:37 -0400

    Added Q562 solutions

--- 562-Longest-Line-Consecutive-One/Q562.py
+++ 562-Longest-Line-Consecutive-One/Q562.py
@@ -1,4 +1,5 @@
-import copy
+# Accepted brute force solution
+
 class Solution(object):
     def longest(self, v):
         ans = 0
@@ -20,25 +21,7 @@ class Solution(object):
         #print ("v={},ans={}".format(v, ans))
         return ans
 
-    def get(self,i,j):
-        # Time limit exceeded if we do the virtual padding here
-        #print ("i={}, j={}".format(i,j))
-        # We already assumed there was a padding done on M.
-        M = self.M
-        N1 = len(M)
-        N2 = len(M[0])
-        if N1 > N2: # padded diff columns to the right
-            diff = N1-N2
-            if j >= N2:
-                return None
-            else:
-                return M[i][j]
-        else: # padded diff rows to the bottom
-            diff = N2-N1
-            if i >= N1:
-                return None
-            else:
-                return M[i][j]
+
 
 
     def longestLine(self, M):
@@ -60,89 +43,231 @@ class Solution(object):
             if t > ans:
                 ans = t
 
-
-        N1 = len(M)
-        N2 = len(M[0])
-        diff = 0 # later t shall not exceed len(Mat)-diff
-        '''
-        # Memory limit exceeded if we brutally do all the paddings
-        if N1 > N2:
-            diff = N1 - N2
-            for i in range(N1):
-                M[i] += [None] * diff
-        else:
-
-            diff = N2 - N1
-            M += [[None] * N2] * diff
-        '''
-
-
-        #print ("After padding, M is:{}, N1={}, N2={}".format(M, N1, N2))
-
-        self.M = M
-        N = N1
-        if N2 > N1: N = N2
-        # Forward (/), upper triangle
-        for k in range(N):
-            L = []
-            i = k; j = 0
-            while (i >= 0):
-                L.append(self.get(i,j))
-                i -= 1
-                j += 1
-            t = self.longest(L)
-            if t > ans:
-                if t <= (N-diff):
+        Y = len(M)
+        X = len(M[0])
+        if Y > X:
+            # '/'
+            for k in range(X):
+                i = k; j = 0; L=[]
+                while i >= 0:
+                    L.append(M[i][j])
+                    i -= 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
                     ans = t
-                else:
-                    ans = (N-diff)
-
-        # Forward (/), lower triangle
-        for k in range(N):
-            L = []
-            i = N-1; j = N-k-1
-            while (j < N):
-                L.append(self.get(i,j))
-                i -= 1
-                j += 1
-            #print ("/: len(L)={}".format(len(L)))
-            t = self.longest(L)
-            if t > ans:
-                if t <= (N-diff):
+            for k in range(Y-X):
+                i = X+k; j=0; L=[]
+                while j < X:
+                    L.append(M[i][j])
+                    i -= 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
                     ans = t
-                else:
-                    ans = N - diff
-
-        # Backward (\), upper triangle
-        for k in range(N):
-            L = []
-            i = 0; j = N-k-1
-            while (j < N):
-                L.append(self.get(i,j))
-                i += 1
-                j += 1
-            t = self.longest(L)
-            #print ("L={}, t={}".format(L, t))
-            if t > ans:
-                if t <= (N-diff):
+            for k in range(1, X):
+                i = Y-1; j = k; L=[]
+                while (j < X):
+                    L.append(M[i][j])
+                    i -= 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
                     ans = t
-                else:
-                    ans = (N-diff)
-
-        # Backward (\), lower triangle
-        for k in range(N):
-            L = []
-            i = N-k-1; j=0
-            while (j <= k):
-                L.append(self.get(i,j))
-                i += 1
-                j += 1
-            #print ("\: len(L)={}".format(len(L)))
-            t = self.longest(L)
-            if t > ans:
-                if t <= (N-diff):
+            # '\'
+            for k in range(X):
+                i = 0; j = X-k-1; L=[]
+                while (j < X):
+                    L.append(M[i][j])
+                    i += 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
                     ans = t
-                else:
-                    ans = (N-diff)
+            for k in range(1, Y-X):
+                i = k; j = 0; L = []
+                while (j < X):
+                    #print ("i={}, j={}, len(M)={}, len(M[0])={}".format(i,j,len(M), len(M[0])))
+                    L.append(M[i][j])
+                    i += 1
+                    j += 1
+
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+            for k in range(X):
+                i = Y-k-1; j = 0; L = []
+                while (i < Y):
+                    L.append(M[i][j])
+                    i += 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+
+        else: # Y <= X
+            # '/'
+            for k in range(Y):
+                i=k; j=0; L=[]
+                while (i>=0):
+                    L.append(M[i][j])
+                    i -= 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+
+            for k in range(1, X-Y):
+                i = Y-1; j = k; L=[]
+                while (i >= 0):
+                    L.append(M[i][j])
+                    i -= 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+
+            for k in range(Y):
+                i=Y-1; j=X-1-k; L=[]
+                while (j < X):
+                    L.append(M[i][j])
+                    i -= 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+
+            # '\'
+            for k in range(Y):
+                i=0; j=X-1-k; L=[]
+                while (j < X):
+                    L.append(M[i][j])
+                    i += 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+
+            for k in range(1, X-Y):
+                i = 0; j = k; L = []
+                while (i < Y):
+                    L.append(M[i][j])
+                    i += 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+
+            for k in range(Y):
+                i = k; j = 0; L = []
+                while (i < Y):
+                    L.append(M[i][j])
+                    i += 1
+                    j += 1
+                t = self.longest(L)
+                if t > ans:
+                    ans = t
+
         return ans
-            
+
+
+# Note1: Here is cleverer brute force solution: We do not need to seperate the cases!
+public class Solution {
+    public int longestLine(int[][] M) {
+        if (M.length == 0)
+            return 0;
+        int ones = 0;
+        //horizontal
+        for (int i = 0; i < M.length; i++) {
+            int count = 0;
+            for (int j = 0; j < M[0].length; j++) {
+                if (M[i][j] == 1) {
+                    count++;
+                    ones = Math.max(ones, count);
+                } else
+                    count = 0;
+            }
+        }
+        //vertical
+        for (int i = 0; i < M[0].length; i++) {
+            int count = 0;
+            for (int j = 0; j < M.length; j++) {
+                if (M[j][i] == 1) {
+                    count++;
+                    ones = Math.max(ones, count);
+                } else
+                    count = 0;
+            }
+        }
+        //upper diagonal
+        for (int i = 0; i < M[0].length || i < M.length; i++) {
+            int count = 0;
+            for (int x = 0, y = i; x < M.length && y < M[0].length; x++, y++) {
+                if (M[x][y] == 1) {
+                    count++;
+                    ones = Math.max(ones, count);
+                } else
+                    count = 0;
+            }
+        }
+        //lower diagonal
+        for (int i = 0; i < M[0].length || i < M.length; i++) {
+            int count = 0;
+            for (int x = i, y = 0; x < M.length && y < M[0].length; x++, y++) {
+                if (M[x][y] == 1) {
+                    count++;
+                    ones = Math.max(ones, count);
+                } else
+                    count = 0;
+            }
+        }
+        //upper anti-diagonal
+        for (int i = 0; i < M[0].length || i < M.length; i++) {
+            int count = 0;
+            for (int x = 0, y = M[0].length - i - 1; x < M.length && y >= 0; x++, y--) {
+                if (M[x][y] == 1) {
+                    count++;
+                    ones = Math.max(ones, count);
+                } else
+                    count = 0;
+            }
+        }
+        //lower anti-diagonal
+        for (int i = 0; i < M[0].length || i < M.length; i++) {
+            int count = 0;
+            for (int x = i, y = M[0].length - 1; x < M.length && y >= 0; x++, y--) {
+                //System.out.println(x+" "+y);
+                if (M[x][y] == 1) {
+                    count++;
+                    ones = Math.max(ones, count);
+                } else
+                    count = 0;
+            }
+        }
+        return ones;
+
+    }
+}
+
+# Note 2: A Dynamic Programming Solution only traverses the matrix once, and keep track of the horizontal, vertical, diagonal and anti-diagonal consecutive 1's up till now. O(nm) time, O(nm) space.
+
+public class Solution {
+    public int longestLine(int[][] M) {
+        if (M.length == 0)
+            return 0;
+        int ones = 0;
+        int[][][] dp = new int[M.length][M[0].length][4];
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[0].length; j++) {
+                if (M[i][j] == 1) {
+                    dp[i][j][0] = j > 0 ? dp[i][j - 1][0] + 1 : 1;
+                    dp[i][j][1] = i > 0 ? dp[i - 1][j][1] + 1 : 1;
+                    dp[i][j][2] = (i > 0 && j > 0) ? dp[i - 1][j - 1][2] + 1 : 1;
+                    dp[i][j][3] = (i > 0 && j < M[0].length - 1) ? dp[i - 1][j + 1][3]+1 : 1;
+                    ones = Math.max(ones, Math.max(Math.max(dp[i][j][0], dp[i][j][1]), Math.max(dp[i][j][2], dp[i][j][3])));
+                }
+            }
+        }
+        return ones;
+    }
+}
